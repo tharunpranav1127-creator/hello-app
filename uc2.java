aacbbf8cda7ff01a79ce850e072dd7cddf9c9343
tharunpranav1127 <tharunpranav.11.27.@gmail.com>
2026-02-19 15:48:54 +0530

    feat: add uc2

---
+++ uc2.java
@@ -0,0 +1,11 @@
+public class uc2{
+    public static void main(){
+        System.out.println("    ***   " + "   ***   " + "******  " + "   *****  ");
+        System.out.println("  **   ** " + " **   ** " + "**    **" + " **       "); 
+        System.out.println(" **     **" + "**     **" + "**    **" + " **       ");
+        System.out.println(" **     **" + "**     **" + "******  " + "   *****  ");
+        System.out.println(" **     **" + "**     **" + "**      " + "       ** ");
+        System.out.println("  **   ** " + " **   ** " + "**      " + " **    ** ");
+        System.out.println("    ***   " + "   ***   " + "**      " + "   *****  ");
+    }
+}
