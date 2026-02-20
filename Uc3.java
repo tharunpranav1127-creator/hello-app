3c45310ba623e2520bd6c65876f14d03d0d13800
tharunpranav1127-creator <tharunpranav.11.27@gmail.com>
2026-02-20 10:47:06 +0530

    Add files via upload

---
+++ Uc3.java
@@ -0,0 +1,16 @@
+public class Uc3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "    ***    ", "    ***    ", "  *****    ", "  *****   "));
+        System.out.println(String.join(" ", "  **   **  ", "  **   **  ", "  **    ** ", " **    ** "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", "  **    ** ", " **       "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", "  *****    ", "  *****   "));
+        System.out.println(String.join(" ", " **     ** ", " **     ** ", "  **       ", "       ** "));
+        System.out.println(String.join(" ", "  **   **  ", "  **   **  ", "  **       ", " **   **  "));
+        System.out.println(String.join(" ", "    ***    ", "    ***    ", "  **       ", "  *****   "));
+    }
+}
+
+
+        
